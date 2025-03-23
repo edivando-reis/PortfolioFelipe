ad4b38e0f356a922f5a1366050cf1669f40a920e
edivando-reis <edivando.siqueira@gmail.com>
2025-03-23 14:14:57 -0300

    Logo change

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
@@ -7,23 +6,24 @@ const Logo: React.FC = () => {
   return (
     <Link to="/">
       <motion.div 
-        className=" text-2xl font-montserrat relative"
+        className="text-2xl font-montserrat relative"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.6, delay: 0.2 }}
       >
         <span className="font-light">F.R</span>
-        <motion.span 
-          className="ml-2 text-[8px] font-light tracking-widest uppercase opacity-70"
+        <motion.div 
+          className="text-[8px] font-light tracking-widest uppercase opacity-70 leading-tight"
           initial={{ opacity: 0, x: -10 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6, delay: 0.6 }}
         >
-          Fotografia de arquiteturas
-        </motion.span>
+          <span className="block">Fotografia de</span>
+          <span className="block">arquitetura.</span>
+        </motion.div>
       </motion.div>
     </Link>
   );
 };
 
-export default Logo;
+export default Logo;  
